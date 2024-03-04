49e4c7862185d9f867c83fce007d43c84ef2513b
Manikanta Pindi <131873708+manikanta-pindi@users.noreply.github.com>
2024-03-04 12:34:51 +0530

    branch

--- tina/config.js
+++ tina/config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "tinacms";
 import page from "./collections/page";
 import post from "./collections/post";
 
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
 export const config = defineConfig({
+  branch,
   clientId: "0ad2b629-c4a9-42c8-9e42-b8816dbbecd0",
   token: "4052aa4257da67860d00709c2de0482efc981d55",
   media: {
