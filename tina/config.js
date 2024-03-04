0b4c24617888ebd19706a16b60634b696274bbb3
Manikanta Pindi <131873708+manikanta-pindi@users.noreply.github.com>
2024-03-04 12:42:52 +0530

    token addition

--- tina/config.js
+++ tina/config.js
@@ -6,8 +6,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export const config = defineConfig({
   branch,
-  clientId: "0ad2b629-c4a9-42c8-9e42-b8816dbbecd0",
-  token: "4052aa4257da67860d00709c2de0482efc981d55",
+  clientId: "f1ff8a6e-a407-44e8-9d31-526fa5240c80",
+  token: "00c97d350da76857840e4c3757e3723f4fd97c16",
   media: {
     // If you wanted cloudinary do this
     // loadCustomStore: async () => {
